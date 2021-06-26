b9a46e0c1ee99ae6c6d2e9ce8409e927886f6785
joangie <practicatto@gmail.com>
2021-06-26 18:42:52 -0500

    👾 Initial commit for .asm | colors and description

---
+++ game_of_life.asm
@@ -0,0 +1,44 @@
+#
+#					▒█▀▀█ █▀▀█ █▀▄▀█ █▀▀ 　 █▀▀█ █▀▀ 　 █░░ ░▀░ █▀▀ █▀▀ 
+#					▒█░▄▄ █▄▄█ █░▀░█ █▀▀ 　 █░░█ █▀▀ 　 █░░ ▀█▀ █▀▀ █▀▀ 
+#					▒█▄▄█ ▀░░▀ ▀░░░▀ ▀▀▀ 　 ▀▀▀▀ ▀░░ 　 ▀▀▀ ▀▀▀ ▀░░ ▀▀▀
+# 						Computer Organization's project
+#
+#	 			+-----------------+
+#	 			|     Authors     |			+----------+---------+ 
+#	 			+-----------------+			| Language | Spanish |
+#	 			| Joseph Avila    |			+----------+---------+ 
+#		 		| Joangie Marquez |		
+#		 		+-----------------+		
+#
+#		 	   +--------------------------------------------------------------------------+
+#		 	   |                               Descripcion                                |
+# 			  +--------------------------------------------------------------------------+
+# 			  | Autómata celular (Cellular automaton) que evoluciona de manera discreta. |
+# 			  | Está compuesto por un tablero teóricamente infinito en dos dimensiones   |
+#			  | que rodea las celdas sobre las cuales se producirán las interacciones.   |
+# 			  +--------------------------------------------------------------------------+
+#
+#
+#
+.data
+	#Colores
+	.eqv AZUL 	0x0061A4 #azul son los vivos
+	.eqv PLOMO	0x9FD8FF #muertos
+	.eqv BLANCO	0xFFFFFF #background
+	
+	
+	
+	
+
+
+	
+
+
+
+
+
+
+
+
+
